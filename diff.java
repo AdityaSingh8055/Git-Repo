d6834d69bcbcf7e5cf170a8d3123c7b107e8c040
Aditya Singh <aadigzp03@gmail.com>
2023-02-15 23:25:25 +0530

    add diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String[] args) {
+        int a=9;
+        int b=8;
+        int =a+b;
+        int diff=a+b;
+        System.out.println("My sum is"+diff);
+    }
+}
--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class sum{
         int a=9;
         int b=8;
         int sum=a+b;
-        System.out.println(sum);
+        System.out.println("My sum is"+sum);
     }
 }
